c3a57b7c94e305717c5115abc27f44edfd250739
Sean Madondo <seanmadondo@yahoo.com>
2021-06-16 09:22:56 +1200

    ticket adjustments

--- src/components/Ticket/index.tsx
+++ src/components/Ticket/index.tsx
@@ -1,13 +1,31 @@
 /** @jsxImportSource @emotion/react */
 // import { css } from "@emotion/react";
 
-import { Box, Card, CardHeader } from "@material-ui/core";
+import {
+  Box,
+  Card,
+  CardHeader,
+  CardContent,
+  CardActions,
+} from "@material-ui/core";
 
 export const Ticket = (): JSX.Element => {
   return (
     <Box>
-      <Card css={{ width: "20%", borderRadius: "10px" }}>
-        <CardHeader css={{ fontSize: "5px" }} title="Ticket Title" />
+      <Card
+        css={{
+          width: "20%",
+          borderRadius: "10px",
+          fontFamily: "'Baloo 2', cursive",
+        }}
+      >
+        <CardHeader title="Ticket Title" disableTypography />
+        <CardContent>
+          <div>Description to go here</div>
+        </CardContent>
+        <CardActions>
+          <div>Actions to go here</div>
+        </CardActions>
       </Card>
     </Box>
   );
